03b1d16b948d5f5b07674d32d82f650a636cee88
sophiegee <134379147+sophiegee@users.noreply.github.com>
2023-07-15 10:58:25 +0200

    add 2.3. JavaScript DropDown Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log('Halli Hallo Hallöle!')
+function menuToggle(params) {
+    var x = document.getElementById ('myNavtoggle')
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle'
+    }
+}

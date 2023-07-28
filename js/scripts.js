8cecf6727bbb8eaa2c4c8f8eee5ddf56798af85f
sophiegee <134379147+sophiegee@users.noreply.github.com>
2023-07-28 12:09:13 +0200

    add Test Preperation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function menuToggle(params) {
     var x = document.getElementById ('myNavtoggle')
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
         x.className += ' responsive';
     } else {
         x.className = 'navtoggle'
